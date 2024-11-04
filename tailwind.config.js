03791b96fe0b821910925e98fde3cc0547c614c5
Ashish jha <ajha5645@gmail.com>
2024-11-04 23:19:01 +0530

    this commit wil add the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'], theme: {
-    extend: {},
-  }, plugins: [],
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#171717', // Black
+        secondary: '#ededed', // whitish
+        accent: '#F59E0B', // Amber
+        neutral: '#9b9b9b', // Slate
+        base: '#F3F4F6', // Gray
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [],
 }
