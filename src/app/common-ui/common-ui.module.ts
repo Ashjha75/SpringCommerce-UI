23666fb199f103ecd4384ac48789f2c63fa77644
Ashish jha <ajha5645@gmail.com>
2024-11-04 23:35:15 +0530

    this commit wil add extral files need generated by angular cli

---
+++ src/app/common-ui/common-ui.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommonUiRoutingModule } from './common-ui-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommonUiRoutingModule
+  ]
+})
+export class CommonUiModule { }
---
+++ src/app/consumer-ui/consumer-ui.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConsumerUiRoutingModule } from './consumer-ui-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConsumerUiRoutingModule
+  ]
+})
+export class ConsumerUiModule { }
