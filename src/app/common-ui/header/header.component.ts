786df0cf2f47a1c0cd86bbfe4fde258ed3f5c412
Ashish jha <ajha5645@gmail.com>
2024-11-05 13:29:21 +0530

    this commit wil improve the ui of header

--- src/app/common-ui/header/header.component.ts
+++ src/app/common-ui/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  standalone: true,
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  searchPlaceholder: string = 'Search for products';
+  private placeholderOptions: string[] = ['Search for products', 'Search for kurtti', 'Search for jeans', 'Search for brands', 'Search for anything'];
+  private placeholderIndex: number = 0;
+  private intervalId: any;
 
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.placeholderIndex = (this.placeholderIndex + 1) % this.placeholderOptions.length;
+      this.searchPlaceholder = this.placeholderOptions[this.placeholderIndex];
+    }, 1200);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
