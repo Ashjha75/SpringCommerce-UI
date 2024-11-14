44e298c9aeb531a4c58b3090d9b3cdac8431ac9a
Ashish jha <ajha5645@gmail.com>
2024-11-14 16:03:38 +0530

    This commit will add the new comonent and auth service for the process of routes

--- src/app/common-ui/arrow-slider/arrow-slider.component.ts
+++ src/app/common-ui/arrow-slider/arrow-slider.component.ts
@@ -66,7 +66,7 @@ export class ArrowSliderComponent implements OnInit, OnDestroy {
   prevSlide() {
     if (this.isAnimating) return;
     this.isAnimating = true;
-    // this.addAnimationClass(this.settings.slideDir === 'horizontal' ? 'slide-in-right' : 'slide-in-top');
+    this.addAnimationClass(this.settings.slideDir === 'horizontal' ? 'slide-in-right' : 'slide-in-top');
     this.currentIndex = this.currentIndex > 0 ? this.currentIndex - 1 : this.getCardBatches().length - 1;
     this.resetAutoSlide();
   }
@@ -74,7 +74,7 @@ export class ArrowSliderComponent implements OnInit, OnDestroy {
   nextSlide() {
     if (this.isAnimating) return;
     this.isAnimating = true;
-    // this.addAnimationClass(this.settings.slideDir === 'horizontal' ? 'slide-in-left' : 'slide-in-bottom');
+    this.addAnimationClass(this.settings.slideDir === 'horizontal' ? 'slide-in-left' : 'slide-in-bottom');
     this.currentIndex = this.currentIndex < this.getCardBatches().length - 1 ? this.currentIndex + 1 : 0;
     this.resetAutoSlide();
   }
@@ -95,12 +95,12 @@ export class ArrowSliderComponent implements OnInit, OnDestroy {
     return batches;
   }
 
-  // addAnimationClass(className: string) {
-  //   const sliderContainer = this.el.nativeElement.querySelector('.slider');
-  //   this.renderer.addClass(sliderContainer, className);
-  //   setTimeout(() => {
-  //     this.renderer.removeClass(sliderContainer, className);
-  //     this.isAnimating = false;
-  //   }, 1000);
-  // }
+  addAnimationClass(className: string) {
+    const sliderContainer = this.el.nativeElement.querySelector('.slider');
+    this.renderer.addClass(sliderContainer, className);
+    setTimeout(() => {
+      this.renderer.removeClass(sliderContainer, className);
+      this.isAnimating = false;
+    }, 1000);
+  }
 }
